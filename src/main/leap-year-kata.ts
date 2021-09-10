b7284109f1ea33344bba713b804dc2a81226fce0
TPH <thu-phuong.ho@thu-phuong.ho.macbook>
2021-09-10 11:59:28 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/leap-year-kata.ts
+++ src/main/leap-year-kata.ts
@@ -1,15 +1,29 @@
+export function isLeapYear(year:number):boolean{
 
-
-export function isLeapYear(year:number){
-
-    if (year % 400 == 0){
+    if(isAtypicalLeapYear(year)){
         return true;
     }
 
-    if (year % 4 ==0){
-        return true;
+    if(isAtypicalCommonYear(year)){
+        return false;
     }
 
+    if(isTypicalLeapYear(year)){
+        return true;
+    }
+    return false;
+}
 
+function isAtypicalLeapYear(year:number):boolean{
 
+        return year % 400 == 0;
+    }
+
+function isAtypicalCommonYear(year:number):boolean{
+
+    return year % 100 == 0;
+}
+function isTypicalLeapYear(year:number):boolean{
+
+        return year % 4 ==0;
 }
--- src/test/leap-year-kata.test.ts
+++ src/test/leap-year-kata.test.ts
@@ -6,16 +6,18 @@ describe("Leap Year Tests",()=>{
         expect(result).toBe(true);
     })
 
-    it.each([1200, 1600, 2000])("should return false when $d is an atypical leap year",(year)=>{
+    it.each([1200, 1600, 2000])("should return true when $d is an atypical leap year",(year)=>{
         const result = isLeapYear(year)
-
         expect(result).toBe(true)
     })
 
+    it.each([1900, 1700, 1800])("should return false if $d is an atypical common year",(year)=>{
+        const result = isLeapYear(year)
+        expect(result).toBe(false)
+    })
 
-    // it.each([1700])("should return false when $d is an atypical common year",(year)=>{
-    //     const result = isLeapYear(year)
-    //
-    //     expect(result).toBe(false)
-    // })
+    it.each([2001, 2002, 2003])("should return false if $d is a typical common year",(year)=>{
+        const result = isLeapYear(year)
+        expect(result).toBe(false)
+    })
 })
